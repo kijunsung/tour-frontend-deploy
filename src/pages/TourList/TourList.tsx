9db247719552be3200512d9fa6db18f7f56affc7
kijunsung <keejunsd@aver.com>
2025-07-28 09:57:52 +0900

    마지막

--- src/pages/TourList/TourList.tsx
+++ src/pages/TourList/TourList.tsx
@@ -288,7 +288,7 @@ const TourList: React.FC = () => {
           💬 나의 여행 계획
         </Typography>
         <Typography variant="h6" mt={1}>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;나만의 여행 계획을 관리해보세요&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        나만의 여행 계획을 관리해보세요
         </Typography>
       </Box>
 
@@ -325,11 +325,11 @@ const TourList: React.FC = () => {
           </Button>
         </Box>
       ) : (
-        <Grid container spacing={3}>
+        <Grid container spacing={1}>
           {tours.map((tour) => (
             <Box 
             sx={{ 
-              width: { xs: '100%', sm: '50%', lg: '33.33%' },
+              width: { xs: '100%', sm: '50%', lg: '32.74%' },
               padding: 1 
             }} 
             key={tour.tourId}
